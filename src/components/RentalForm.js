c416dcc241d89f15c4247a23053d80e6bf6f777b
nolbuzanis <nolanbuzanis@gmail.com>
2019-04-02 15:06:50 -0400

    Added handle submit function to form. Currently outputs to console.log

--- src/components/RentalForm.js
+++ src/components/RentalForm.js
@@ -4,33 +4,39 @@ import { Field, reduxForm } from 'redux-form';
 
 class RentalForm extends React.Component {
   renderInput(formProps) {
-    console.log(formProps);
     return (
       <div>
-        <label>{formProps.input.name}</label>
-        <input
-          onChange={formProps.input.onChange}
-          value={formProps.input.value}
-        />
+        <label>{formProps.label}</label>
+        <input {...formProps.input} />
       </div>
     );
   }
 
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
     return (
-      <form className='ui form'>
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className='ui form'
+      >
         <Field
           className='field'
           name='name'
           component={this.renderInput}
           type='text'
+          label='Name'
         />
         <Field
           className='field'
           name='email'
           component={this.renderInput}
           type='text'
+          label='Email'
         />
+        <button className='ui primary button'>Submit</button>
       </form>
     );
   }
