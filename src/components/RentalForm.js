78e4dff364ef67256ddc7bb74e445db84df68f01
nolbuzanis <nolanbuzanis@gmail.com>
2019-04-02 15:59:09 -0400

    Added validate and error messages, updated styling of inputs and submit button

--- src/components/RentalForm.js
+++ src/components/RentalForm.js
@@ -3,45 +3,76 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 
 class RentalForm extends React.Component {
-  renderInput(formProps) {
+  showErrors = ({ touched, error }) => {
+    if (touched && error) {
+      return <div style={{ color: '#9f3a38' }}>{error}</div>;
+    }
+    return null;
+  };
+
+  renderInput = formProps => {
     return (
-      <div>
-        <label>{formProps.label}</label>
-        <input {...formProps.input} />
+      <div
+        className={`field ${
+          formProps.meta.error && formProps.meta.touched ? 'error' : ''
+        }`}
+      >
+        <label
+          style={{
+            color: 'white'
+          }}
+        >
+          {formProps.label}
+        </label>
+        <input {...formProps.input} autoComplete='off' />
+        {this.showErrors(formProps.meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValues) {
-    console.log(formValues);
+    //validate(formValues);
   }
 
   render() {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className='ui form'
+        className='ui form error'
       >
         <Field
-          className='field'
           name='name'
           component={this.renderInput}
           type='text'
           label='Name'
         />
         <Field
-          className='field'
           name='email'
           component={this.renderInput}
           type='text'
           label='Email'
         />
-        <button className='ui primary button'>Submit</button>
+        <button className='ui teal button'>Submit</button>
       </form>
     );
   }
 }
 
-const rentalForm = reduxForm({ form: 'rent' })(RentalForm);
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.name) {
+    errors.name = 'Required';
+  }
+  if (!formValues.email) {
+    errors.email = 'Required';
+  } else if (
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formValues.email)
+  ) {
+    errors.email = 'Invalid email address';
+  }
+  return errors;
+};
+
+const rentalForm = reduxForm({ form: 'rent', validate })(RentalForm);
 
 export default connect()(rentalForm);
