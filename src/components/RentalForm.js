ba844ef7b1ae86bf4f253bf66f380e0650f3c4cb
nolbuzanis <nolanbuzanis@gmail.com>
2019-04-03 13:42:26 -0400

    Set up Datepicker components with custom helper methods for automatically ensuring the start date is never after the end date, etc..

--- src/components/RentalForm.js
+++ src/components/RentalForm.js
@@ -2,13 +2,60 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { submitOrder } from '../actions/index';
+import DatePicker from 'react-datepicker';
+import './RentalForm.css';
 
 class RentalForm extends React.Component {
+  state = { startDate: new Date(), endDate: new Date() };
+
   showErrors = ({ touched, error }) => {
     if (touched && error) {
       return <div style={{ color: '#9f3a38' }}>{error}</div>;
     }
     return null;
+  };
+
+  getNextDate(date) {
+    const currentDay = date.getDate();
+    const currentMonth = date.getMonth();
+    const currentYear = date.getFullYear();
+
+    return new Date(currentYear, currentMonth, currentDay + 1);
+  }
+
+  getPreviousDate(date) {
+    const currentDay = date.getDate();
+    const currentMonth = date.getMonth();
+    const currentYear = date.getFullYear();
+
+    return new Date(currentYear, currentMonth, currentDay - 1);
+  }
+
+  componentDidMount() {
+    this.setState({
+      endDate: this.getNextDate(this.state.startDate)
+    });
+  }
+
+  handleChangeStart = e => {
+    this.setState({
+      startDate: e
+    });
+    if (!(e.getTime() < this.state.endDate.getTime())) {
+      this.setState({
+        endDate: this.getNextDate(e)
+      });
+    }
+  };
+
+  handleChangeEnd = e => {
+    this.setState({ endDate: e });
+    console.log(e.getTime() <= this.state.startDate.getTime());
+    if (e.getTime() < this.state.startDate.getTime()) {
+      this.setState({
+        startDate: this.getPreviousDate(e)
+      });
+    }
   };
 
   renderInput = formProps => {
@@ -64,6 +111,20 @@ class RentalForm extends React.Component {
           component={this.renderInput}
           type='text'
           label='Address'
+        />
+        <DatePicker
+          selected={this.state.startDate}
+          selectsStart
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeStart}
+        />
+        <DatePicker
+          selected={this.state.endDate}
+          selectsEnd
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeEnd}
         />
         <button className='ui teal button'>Submit</button>
       </form>
