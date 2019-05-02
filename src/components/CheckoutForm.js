1acb624403f3c1a58a95b29510652dc3a183360a
nolbuzanis <nolanbuzanis@gmail.com>
2019-05-01 22:22:23 -0400

    Fixed modal to close when clicking outside it. Changed font to size 16

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -11,10 +11,19 @@ class CheckoutForm extends React.Component {
       history.push('/rent');
       return null;
     }
-    console.log(this.props.info);
     return ReactDOM.createPortal(
-      <div className='modal-container'>
-        <div className='modal-body'>
+      <div
+        className='modal-container'
+        onClick={() => {
+          history.push('/rent');
+        }}
+      >
+        <div
+          className='modal-body'
+          onClick={e => {
+            e.stopPropagation();
+          }}
+        >
           <h2>Order Summary</h2>
           <ul className='summary-list'>
             <li>Name:{' ' + this.props.info.name}</li>
@@ -29,13 +38,12 @@ class CheckoutForm extends React.Component {
               Pickup on:
               {' ' + this.props.info.enddate.toString().substring(0, 15)}
             </li>
-            <li>Product:{' ' + this.props.info.endDate}</li>
+            <li>Product:{' ' + this.props.info.package}</li>
             <li>
               <h3>Total:{' $' + this.props.info.price.toString()}</h3>
             </li>
           </ul>
           <div>
-            <p>Would you like to complete this purchase?</p>
             <CardElement />
             <button onClick={() => console.log('Clicked!')}>Submit</button>
           </div>
