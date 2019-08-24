b92a0ccc580e648e329912d60b39c31636e5ff20
nolbuzanis <nolanbuzanis@gmail.com>
2019-08-24 18:16:10 -0400

    Changed stripe provider

--- src/components/RentalForm.js
+++ src/components/RentalForm.js
@@ -263,7 +263,7 @@ class RentalForm extends React.Component {
             />
           </ul>
         </div> */}
-          <StripeProvider apiKey={stripePublicKey}>
+          <StripeProvider stripe={stripePublicKey}>
             <Elements>
               <CheckoutForm daysRented={this.state.daysRented} />
             </Elements>
